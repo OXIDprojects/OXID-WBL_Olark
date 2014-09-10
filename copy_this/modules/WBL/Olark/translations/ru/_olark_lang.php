d54ab10b518a048dd30e60e093d2a7df049220ba
kermie <marco.steinhaeuser@gmail.com>
2014-09-10 21:26:37 +0200

    added RU translations

---
+++ copy_this/modules/WBL/Olark/translations/ru/_olark_lang.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * ./modules/WBL/Olark/translations/de/_olark_lang.php
+     * @author     Bjoern Simon Lange <code@wbl-konzept.de>
+     * @category   modules
+     * @package    WBL_Olark
+     * @subpackage translations
+     * @version    SVN: $Id$
+     */
+
+    /**
+     * German language file.
+     * @author     Bjoern Simon Lange <code@wbl-konzept.de>
+     * @category   modules
+     * @package    WBL_Olark
+     * @subpackage translations
+     * @version    SVN: $Id$
+     */
+
+    return $aLang = array(
+        'charset'           => 'UTF-8',
+        'WBL_OLARK_NS_LINK' => 'Вопросы? Идеи?'
+    );
